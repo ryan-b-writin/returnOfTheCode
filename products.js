7f076c6450394393c5ec04b3801d321e7e4c8c38
Andi Devois <devoisaurus@Andi-Devoiss-MacBook-Pro.local>
2016-02-11 17:31:33 -0600

    javascript loop

--- products.js
+++ products.js
@@ -1,66 +1,74 @@
-var productArray [
+var productArray = [
     {  	title: "Gymkata",
-    	  imageURL: "placeholder",
+    	imageURL: "img/gymkata.jpg",
         inStock: false,
         description: "Brooklyn health goth portland 8-bit chambray.",
-        price: "$5 + shipping*";
+        price: "$5 + shipping*"
 
     },
 
     {  	title: "Lethal Weapon 2",
-    	  imageURL: "placeholder",
+    	imageURL: "img/lethalweapon.jpg",
         inStock: false,
         description: "Brooklyn health goth portland 8-bit chambray.",
-        price: "$5 + shipping*";
+        price: "$5 + shipping*"
 
     },
 
     {  	title: "Manos: Hands of Fate",
-    	  imageURL: "placeholder",
+    	imageURL: "img/manos.jpg",
         inStock: false,
         description: "Brooklyn health goth portland 8-bit chambray.",
-        price: "$5 + shipping*";
+        price: "$5 + shipping*"
 
     },
     	
     {  	title: "Troll 2",
-    	  imageURL: "placeholder",
+    	imageURL: "img/troll.jpg",
         inStock: false,
         description: "Brooklyn health goth portland 8-bit chambray.",
-        price: "$5 + shipping*";
+        price: "$5 + shipping*"
 
     },
     	
     {  	title: "Howard the Duck",
-    	imageURL: "placeholder",
+    	imageURL: "img/howard.jpg",
         inStock: false,
         description: "Brooklyn health goth portland 8-bit chambray.",
-        price: "$5 + shipping*";
+        price: "$5 + shipping*"
 
     },
 
     { 	title: "Garbage Pail Kids",
-    	imageURL: "placeholder",
+    	imageURL: "img/garbage.jpg",
         inStock: false,
         description: "Brooklyn health goth portland 8-bit chambray.",
-        price: "$5 + shipping*";
+        price: "$5 + shipping*"
 
     },
 
     { 	title: "Touch of Satan",
-    	imageURL: "placeholder",
+    	imageURL: "img/satan.jpg",
         inStock: false,
         description: "Brooklyn health goth portland 8-bit chambray.",
-        price: "$5 + shipping*";
+        price: "$5 + shipping*"
 
     },
 
     { 	title: "Glitter",    
-    	imageURL: "placeholder",
+    	imageURL: "img/glitter.png",
         inStock: true,
         description: "Brooklyn health goth portland 8-bit chambray.",
-        price: "$5 + shipping*";
+        price: "$5 + shipping*"
 
     },
 
-]
+];
+
+var products = document.getElementById("videos");
+
+for (var i = 0; i < productArray.length; i++) {
+    products.innerHTML += '<article>' + productArray[i].title +
+    "<img src='"+ productArray[i].imageURL +"'>" + '</article>';
+
+};
